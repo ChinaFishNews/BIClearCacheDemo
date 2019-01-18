7b1f3987ae85bd94c29035b6baa62e1b9504190a
yuxinwen <yuxinwen@baidu.com>
2019-01-18 11:55:10 +0800

    Initial Commit

---
+++ BIClearCacheDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BIClearCacheDemo
+//
+//  Created by xinwen on 2019/1/18.
+//  Copyright © 2019年 baidu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BIClearCacheDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BIClearCacheDemo
+//
+//  Created by xinwen on 2019/1/18.
+//  Copyright © 2019年 baidu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
