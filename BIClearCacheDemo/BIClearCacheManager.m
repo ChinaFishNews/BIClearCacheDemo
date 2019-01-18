5049a90cb9426887cedfa20f1ca374b507de3933
wuning <wuning03@baidu.com>
2019-01-18 17:19:59 +0800

    [0.0.1][other][clear cache util method][cache]

--- BIClearCacheDemo/BIClearCacheManager.m
+++ BIClearCacheDemo/BIClearCacheManager.m
@@ -146,9 +146,16 @@
     
 }
 
+//单位：MB
 - (NSNumber *)cacheSizeOfModule:(BIModuleName)module
 {
-    return @0;
+    BIClearCacheModel *model = _moduleArray[[self indexOfModule:module]];
+    
+    long long totalFileSize = [self totalFileSizeAtPathArray:model.cacheFilePath];
+    
+    long long totalDirSize = [self totalDirSizeAtPathArray:model.cacheDirPath];
+    
+    return @((totalFileSize + totalDirSize)/1024.0/1024.0);
 }
 
 - (void)clearCacheWithModule:(BIModuleName)module complete:(void (^)(void))complete
@@ -157,6 +164,61 @@
 }
 
 #pragma mark - util method
+
+- (long long)totalFileSizeAtPathArray:(NSArray<NSString *> *)array
+{
+    long long result = 0;
+    for (NSString *filePath in array)
+    {
+        long long fileSize = [self fileSizeAtPath:filePath];
+        result += fileSize;
+    }
+    return result;
+}
+
+- (long long)totalDirSizeAtPathArray:(NSArray<NSString *> *)array
+{
+    long long result = 0;
+    for (NSString *dirPath in array)
+    {
+        long long dirSize = [self folderSizeAtPath:dirPath];
+        result += dirSize;
+    }
+    return result;
+}
+
+//单位：B
+- (long long)fileSizeAtPath:(NSString *)path
+{
+    NSFileManager *fileManager=[NSFileManager defaultManager];
+    if([fileManager fileExistsAtPath:path]){
+        long long size=[fileManager attributesOfItemAtPath:path error:nil].fileSize;
+        return size;
+    }
+    return 0;
+}
+
+//单位：B
+- (long long)folderSizeAtPath:(NSString *)path
+{
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+    NSString *cachePath = [NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES) firstObject];
+    cachePath = [cachePath stringByAppendingPathComponent:path];
+    long long folderSize = 0;
+    if ([fileManager fileExistsAtPath:cachePath])
+    {
+        NSArray *childerFiles = [fileManager subpathsAtPath:cachePath];
+        for (NSString *fileName in childerFiles)
+        {
+            NSString *fileAbsolutePath = [cachePath stringByAppendingPathComponent:fileName];
+            long long size = [self fileSizeAtPath:fileAbsolutePath];
+            folderSize += size;
+        }
+        return folderSize;
+    }
+    return 0;
+}
+
 - (void)initPlistFile
 {
     //本地没有则拷贝plist，文件到Doc目录
