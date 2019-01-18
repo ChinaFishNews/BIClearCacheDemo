edda480bc95065c5862497e98fc6376d280d01fa
wuning <wuning03@baidu.com>
2019-01-18 17:47:39 +0800

    [0.0.1][other][clear cache util method][cache]

--- BIClearCacheDemo/BIClearCacheManager.m
+++ BIClearCacheDemo/BIClearCacheManager.m
@@ -158,12 +158,47 @@
     return @((totalFileSize + totalDirSize)/1024.0/1024.0);
 }
 
-- (void)clearCacheWithModule:(BIModuleName)module complete:(void (^)(void))complete
+- (void)clearCacheWithModule:(BIModuleName)module complete:(void (^)(void))completeBlock
 {
+    __weak BIClearCacheManager *weakself = self;
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        BIClearCacheModel *model = self.moduleArray[[weakself indexOfModule:module]];
+    
+        //删文件和索引
+        //Todo:删索引
+        [weakself deleteFilesAtPathArray:model.cacheFilePath];
+        //删目录和索引
+        //Todo:删索引
+        [weakself deleteFilesAtPathArray:model.cacheDirPath];
+        
+        //返回主线程
+        dispatch_async(dispatch_get_main_queue(), ^{
+            if (completeBlock) {
+                completeBlock();
+            }
+        });
+    });
     
 }
 
 #pragma mark - util method
+
+- (void)deleteFilesAtPathArray:(NSArray<NSString *> *)array
+{
+    for (NSString *filePath in array)
+    {
+        [[NSFileManager defaultManager] removeItemAtPath:filePath error:nil];
+    }
+}
+
+- (void)deleteDirsAtPathArray:(NSArray<NSString *> *)array
+{
+    for (NSString *dirPath in array)
+    {
+        //Todo:是否考虑递归
+        [[NSFileManager defaultManager] removeItemAtPath:dirPath error:nil];
+    }
+}
 
 - (long long)totalFileSizeAtPathArray:(NSArray<NSString *> *)array
 {
